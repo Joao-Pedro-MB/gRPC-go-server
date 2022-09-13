cfabdcc529a8cfaf2fc976a98ca0f01dc3709994
Joao Pedro De Moraes Bonucci <joao.bonucci@ifood.com.br>
2022-09-13 12:11:54 -0300

    added vehicle methods

--- vehicle/vehicle.go
+++ vehicle/vehicle.go
@@ -6,14 +6,39 @@ import (
 	context "golang.org/x/net/context"
 )
 
+var listOfCars []Vehicle
+
+type VehicleRegistration struct {
+	Brand string `json:"brand,omitempty"`
+	Model string `json:"model,omitempty"`
+	Year  int32  `json:"year,omitempty"`
+}
+
+type Vehicle struct {
+	Id    int32  `json:"id,omitempty"`
+	Brand string `json:"brand,omitempty"`
+	Model string `json:"model,omitempty"`
+	Year  int32  `json:"year,omitempty"`
+}
+
+type VehicleId struct {
+	VehicleId int32 `json:"vehicleId,omitempty"`
+}
+
 type VehicleServer struct {
 	UnimplementedVehicleServiceServer
 }
 
 func (v *VehicleServer) StoreVehicle(ctx context.Context, vehicleRegistration *VehicleRegistration) (*Vehicle, error) {
-	a := vehicleRegistration.String()
-	fmt.Print(a)
-	return &Vehicle{}, nil
+	vehicle := Vehicle{
+		Id:    int32(len(listOfCars)),
+		Brand: vehicleRegistration.GetBrand(),
+		Model: vehicleRegistration.GetModel(),
+		Year:  vehicleRegistration.GetYear(),
+	}
+	listOfCars = append(listOfCars, vehicle)
+	fmt.Printf("new vehicle added: %v", vehicle.String())
+	return &vehicle, nil
 }
 
 func (v *VehicleServer) UpdateVehicle(ctx context.Context, vehicle *Vehicle) (*Vehicle, error) {
@@ -21,13 +46,24 @@ func (v *VehicleServer) UpdateVehicle(ctx context.Context, vehicle *Vehicle) (*V
 }
 
 func (v *VehicleServer) GetVehicle(ctx context.Context, vehicleId *VehicleId) (*Vehicle, error) {
-	return &Vehicle{}, nil
+	vehicle := &listOfCars[vehicleId.GetVehicleId()]
+	return vehicle, nil
 }
 
+// TODO list vehicles
 func (v *VehicleServer) ListVehicles(ctx context.Context, void *Void) (*Vehicle, error) {
+
+	for _, c := range listOfCars {
+		fmt.Print(c.String())
+	}
 	return &Vehicle{}, nil
 }
 
-func (v *VehicleServer) DeleteVehicle(ctx context.Context, vehicle *VehicleId) (*Void, error) {
+// TODO delete vehicles
+func (v *VehicleServer) DeleteVehicle(ctx context.Context, vehicleId *VehicleId) (*Void, error) {
+	i := vehicleId.GetVehicleId()
+	copy(listOfCars[i:], listOfCars[i+1:])
+	listOfCars[len(listOfCars)-1] = Vehicle{}
+	listOfCars = listOfCars[:len(listOfCars)-1]
 	return &Void{}, nil
 }
